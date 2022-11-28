3c6c583d20fe8119eed0e79afb2c325e293d2bbf
amabilelucia <amabileluciacorreamaby@gmail.com>
2022-11-28 19:00:20 -0300

    feature: Adicionar filme destaque

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import Tmdb from "./Tmdb";
 import "./App.css";
 import MovieRow from "./components/MovieRow";
-import featuredMovie from './components/FeaturedMovie';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
@@ -12,13 +12,16 @@ export default () => {
   useEffect(()=>{
     const loadAll = async () => {
       let list = await Tmdb.getHomelist();
+
       setMovieList(list);
       
       let originals = list.filter(i=>i.slug === 'originals');
       let randomChosen = Math.floor(Math.random() * (originals[0].itens.results.length-1));
       let chosen = originals[0].itens.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
 
-      console.log(chosen);
+      console.log(chosenInfo)
+      setFearturedData(chosenInfo);
 
     }
     loadAll();
@@ -26,8 +29,9 @@ export default () => {
 
   return (
     <div className="page">
+
       {featuredData &&
-        <featuredMovie item={featuredData} />
+        <FeaturedMovie item={featuredData} />
       }
       
       <section className="lists">
